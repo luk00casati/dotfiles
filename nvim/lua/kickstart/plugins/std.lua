43b3bf08a87d53e3f08ddf0d4fc393f0d4a5263c
Luca Casati <luk00casati@gmail.com>
2024-04-15 00:15:29 +0200

    kickstart

---
+++ nvim/lua/kickstart/plugins/std.lua
@@ -0,0 +1,10 @@
+return {
+	-- { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+	-- require("ibl").setup(),
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+}
