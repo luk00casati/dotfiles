22afd80d1677d9075b768bb5645c9118fb21a8e6
Luca Casati <luk00casati@gmail.com>
2024-10-13 23:31:32 +0200

    fixchange1

--- emacs/init.el
+++ emacs/init.el
@@ -1,1 +1,19 @@
-/home/luca/.emacs.d/init.el
+;;load misc.el
+(load "~/.emacs.d/misc.el")
+;;load plugins.el
+(load "~/.emacs.d/plugins.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(modus-vivendi-deuteranopia))
+ '(custom-safe-themes
+   '("1d6b446390c172036395b3b87b75321cc6af7723c7545b28379b46cc1ae0af9e" default))
+ '(package-selected-packages '(elisp-format which-key ivy modus-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
